0a83f0eca7af2294a7bc7b81626195d92fe736b9
Nika <nikaonime@gmail.com>
2023-06-06 09:34:39 +0300

    store, reducer, useDispatch, useSelector, action

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash })
+  }
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash })
+  }
+
+
   return (
     <div className="App">
-
+      <div style={{ fontSize: '3rem' }}>{cash}</div>
+      <button onClick={() => addCash(Number(prompt()))}>add cash</button>
+      <button onClick={() => getCash(Number(prompt()))}>get cash</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const defaultState = {
+    cash: 0
+}
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "ADD_CASH":
+            return { ...state, cash: state.cash + action.payload }
+        case "GET_CASH":
+            return { ...state, cash: state.cash - action.payload }
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
