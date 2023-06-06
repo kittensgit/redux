0529a5bf062cdc9e300684a38a81bdc3f5830133
Nika <nikaonime@gmail.com>
2023-06-06 10:25:56 +0300

    refactoring, combineReducer, redux-devtools

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 
   const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
+  const cash = useSelector(state => state.cash.cash)
 
   const addCash = (cash) => {
     dispatch({ type: "ADD_CASH", payload: cash })
--- src/index.js
+++ src/index.js
@@ -2,25 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-const defaultState = {
-    cash: 0
-}
-
-const reducer = (state = defaultState, action) => {
-    switch (action.type) {
-        case "ADD_CASH":
-            return { ...state, cash: state.cash + action.payload }
-        case "GET_CASH":
-            return { ...state, cash: state.cash - action.payload }
-        default:
-            return state
-    }
-}
-
-const store = createStore(reducer)
+import { store } from './store/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
